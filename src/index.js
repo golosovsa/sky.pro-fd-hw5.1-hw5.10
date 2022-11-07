5f2347d40b2a3ad41fe6b73380b351972491031b
grm <golosov_SA@mail.ru>
2022-11-07 18:12:31 +0300

    mend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./fonts/stratosskyengweb-regular.woff";
+import "./fonts/stratosskyengweb-regular.woff2";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
--- src/old/ButtonText.js
+++
@@ -1,7 +0,0 @@
-function ButtonText(props) {
-    return (
-        <div className={`_btn-text ${props.classes}`}>{props.text}</div>
-    );
-}
-
-export default ButtonText;
